81e4990cf15b3ef414bf7d7c2b5070d0227cacd2
Richard <richard.murri@veracitynetworks.com>
2016-02-17 12:51:49 -0700

    Rewrite to communicate with zulip.

---
+++ src/sentry_zulip/plugin.py
@@ -0,0 +1,102 @@
+"""
+sentry_zulip.plugin
+~~~~~~~~~~~~~~~~~~~
+
+:copyright: (c) 2015 by Sentry Team, see AUTHORS for more details.
+:license: BSD, see LICENSE for more details.
+"""
+import requests
+import sentry_zulip
+
+from django import forms
+from sentry.plugins.bases import notify
+from sentry.http import build_session
+
+
+# Project.get_full_name backported from v8.0
+def get_project_full_name(project):
+    if project.team.name not in project.name:
+        return '%s %s' % (project.team.name, project.name)
+    return project.name
+
+
+class ZulipOptionsForm(notify.NotificationConfigurationForm):
+    apiurl = forms.URLField(
+        help_text='Your custom Zulip api URL',
+        widget=forms.URLInput(attrs={'class': 'span8'}),
+        initial='https://zulip.example.com/api/v1/messages'
+    )
+    username = forms.CharField(
+        label='Bot Name',
+        help_text='The name of the bot.',
+        widget=forms.TextInput(attrs={'class': 'span8'}),
+        initial='sentry@example.com',
+        required=True
+    )
+    apikey = forms.CharField(
+        label='Bot API Key',
+        help_text='The api key of the bot (for authentication).',
+        widget=forms.TextInput(attrs={'class': 'span8'}),
+        required=True
+    )
+    stream = forms.CharField(
+        label='Stream name',
+        help_text='The name of the stream to post messages to.',
+        widget=forms.TextInput(attrs={'class': 'span8'}),
+        initial='Sentry',
+        required=True
+    )
+
+
+class ZulipPlugin(notify.NotificationPlugin):
+    author = 'Richard Murri'
+    author_url = 'https://github.com/richardmurri/sentry-zulip'
+    resource_links = (
+        ('Bug Tracker', 'https://github.com/richardmurri/sentry-zulip/issues'),
+        ('Source', 'https://github.com/richardmurri/sentry-zulip'),
+    )
+
+    title = 'Zulip'
+    slug = 'zulip'
+    description = 'Post notifications to a Zulip stream.'
+    conf_key = 'zulip'
+    version = sentry_zulip.VERSION
+    project_conf_form = ZulipOptionsForm
+
+    def is_configured(self, project):
+        return all((self.get_option(k, project) for k in ('apiurl', 'username', 'apikey', 'stream')))
+
+    def notify(self, notification):
+        event = notification.event
+        group = event.group
+        project = group.project
+
+        if not self.is_configured(project):
+            return
+
+        apiurl = self.get_option('apiurl', project)
+        username = self.get_option('username', project).strip().encode('utf-8')
+        apikey = self.get_option('apikey', project).encode('utf-8')
+        stream = self.get_option('stream', project).strip().encode('utf-8')
+        level = group.get_level_display()
+
+        title = event.message_short.encode('utf-8')
+        project_name = get_project_full_name(project).encode('utf-8')
+
+        values = {
+            'type': 'stream',
+            'to': stream,
+            'subject': title,
+            'content': "[%s] **%s** %s [view](%s)" % (
+                level.upper(), project_name, title, group.get_absolute_url()
+            )
+        }
+
+        # Apparently we've stored some bad data from before we used `URLField`.
+        apiurl = apiurl.strip(' ')
+
+        session = build_session()
+        return session.request(method='GET',
+                               url=apiurl,
+                               data=values,
+                               auth=(username, apikey))
---
+++ tests/sentry_zulip/test_plugin.py
@@ -0,0 +1,44 @@
+from __future__ import absolute_import
+
+import responses
+
+from exam import fixture
+from sentry.models import Rule
+from sentry.plugins import Notification
+from sentry.testutils import TestCase
+from sentry.utils import json
+from urlparse import parse_qs
+
+from sentry_zulip.plugin import ZulipPlugin
+
+
+class ZulipPluginTest(TestCase):
+    @fixture
+    def plugin(self):
+        return ZulipPlugin()
+
+    @responses.activate
+    def test_simple_notification(self):
+        responses.add('GET', 'http://example.com/zulip')
+        self.plugin.set_option('apiurl', 'http://example.com/zulip', self.project)
+        self.plugin.set_option('username', 'bot@example.com', self.project)
+        self.plugin.set_option('apikey', 'testkey', self.project)
+        self.plugin.set_option('stream', 'Sentry', self.project)
+
+        group = self.create_group(message='Hello world', culprit='foo.bar')
+        event = self.create_event(group=group, message='Hello world')
+
+        rule = Rule.objects.create(project=self.project, label='my rule')
+
+        notification = Notification(event=event, rule=rule)
+
+        with self.options({'system.url-prefix': 'http://example.com'}):
+            self.plugin.notify(notification)
+
+        req = responses.calls[0].request
+        assert req.url == 'http://example.com/zulip'
+        data = parse_qs(req.body)
+        assert data['content'][0] == '[ERROR] **foo Bar** Hello world [view](http://example.com/baz/bar/issues/1/)'
+        assert data['to'][0] == 'Sentry'
+        assert data['subject'][0] == 'Hello world'
+        assert data['type'][0] == 'stream'
